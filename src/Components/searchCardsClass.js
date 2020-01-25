0f4d588c518114b581ac309f127af44aafcf6b8a
cnguyen-13 <kpnguyen1013@gmail.com>
2020-01-24 18:06:04 -0600

    functionality to show recipes

--- src/Components/searchCardsClass.js
+++ src/Components/searchCardsClass.js
@@ -1,16 +1,16 @@
-//'https://www.themealdb.com/api/json/v1/1/search.php?s=' search by Name
 import React from 'react';
 
 export class SearchCards extends React.Component {
     render() {
         return (
             <div className="info-card">
-                <img src={this.props.imgSrc} alt={this.props.mealTitle} />
+                <img src={this.props.imgSrc} alt={this.props.mealTitle} width="200" height="200"/>
                 <section className="info-card-description">
                     <h2 className="info-card-title">{this.props.mealTitle}</h2>
                     <p>Category: {this.props.category}</p>
                     <p>Area: {this.props.area}</p>
                     <p>Tags: {this.props.tags}</p>
+                    <button>Get Recipe!</button>
                 </section>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -3,40 +3,56 @@ import ReactDOM from 'react-dom';
 import { SearchCards } from './Components/searchCardsClass';
 import { Navigation } from './Components/navClass';
 
+const categories = ['Beef', 'Breakfast', 'Chicken', 'Dessert', 'Goat', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side', 'Starter', 'Vegan', 'Vegetarian'];
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             filterList: null
         };
-        this.randomMeals = this.randomMeals.bind(this);
+
+        this.showMeals = this.showMeals.bind(this);
         this.getData = this.getData.bind(this);
-        this.randomMeals();
+        this.showMeals();
     }
 
-    //When the page is first loaded, display random meals.
-    //Parse this down further https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
-    async randomMeals() {
+    async showMeals(url, clicked = false) { //Reuse this
         const listMeals = [];
-        const res = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Pasta');
-        const data = await res.json();
-        const meals = await data.meals; //Array
-        for(let i = 0, len = meals.length; i < len; i++) {
-            const searchMeal = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meals[i].strMeal}`);
-            const jsonMeal = await searchMeal.json();
-            const yeet = jsonMeal.meals[0];
-            listMeals.push(<SearchCards mealTitle={yeet.strMeal} category={yeet.strCategory} imgSrc={yeet.strMealThumb} area={yeet.strArea} tags={yeet.strTags} />)
+        if(clicked) { //Shows category/area based on button clicked
+            const res = await fetch(url);
+            const data = await res.json();
+            const meals = data.meals; 
+            for(let i = 0, len = meals.length; i < len; i++) {
+                const searchMeal = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meals[i].strMeal}`);
+                const jsonMeal = await searchMeal.json();
+                const meal = jsonMeal.meals[0];
+                listMeals.push(<SearchCards key={meal.strID} mealTitle={meal.strMeal} category={meal.strCategory} imgSrc={meal.strMealThumb} area={meal.strArea} tags={meal.strTags} />)
+            }
+        } else { //Shows random category when page is visited
+            const randomCategory = categories[Math.floor(Math.random() * 14)];
+            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${randomCategory}`);
+            const data = await res.json();
+            const meals = data.meals; 
+            for(let i = 0, len = meals.length; i < len; i++) {
+                const searchMeal = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meals[i].strMeal}`);
+                const jsonMeal = await searchMeal.json();
+                const meal = jsonMeal.meals[0];
+                listMeals.push(<SearchCards key={meal.strID} mealTitle={meal.strMeal} category={meal.strCategory} imgSrc={meal.strMealThumb} area={meal.strArea} tags={meal.strTags} />)
+            }
         }
 
         this.setState({
-            filterList: <div>
-                {listMeals}
-            </div>
+            filterList: 
+                <div>
+                    {listMeals}
+                </div>
         })
     }
 
     async getData(e, filter) { //Might have to move this function up to App Class
         let baseUrl; 
+        const clicked = true;
         if (filter === 'area') {
             baseUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=';
         } else if (filter === 'cat') {
@@ -50,8 +66,7 @@ class App extends React.Component {
         const res = await fetch(endPoint);
         const data = await res.json();
         const meals = data.meals;
-        console.log(meals)
-        this.setState({filterList: meals})
+        this.showMeals(endPoint, clicked)
     }
 
     render() {
@@ -62,7 +77,6 @@ class App extends React.Component {
                     {this.state.filterList}
                 </div>
             </div>
-            
         );
     }
 };
